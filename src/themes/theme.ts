1677ebed1cdec30038e0ea709d8f24edbdb2d88a
vortex2jm <joaopaulomoura5328@gmail.com>
2022-10-06 11:14:30 -0300

    :sparkles: Adding materialUI theme file

--- src/themes/theme.ts
+++ src/themes/theme.ts
@@ -3,43 +3,44 @@ import { createTheme } from '@mui/material';
 const theme = createTheme({
     palette: {
         primary: {
-            main: '#F52273',
+            main: '#7e4d72',
+            dark: '#662054'
         },
         secondary: {
-            main: '#F5F5F5',
+            main: '#569998',
+            dark: '#396665'
         },
         text: {
             primary: '#F52273',
             secondary: '#6c6767',
         },
     },
-    components: {
-        MuiButton: {
-            styleOverrides: {
-                root: {
-                    color: '#FFF',
-                    borderRadius: '0',
-                    backgroundColor: '#F52273',
-                    textTransform: 'none',
-                    fontWeight: 'bold',
-                    paddingTop: '12px',
-                    paddingBottom: '12px',
-                    ":hover": {
-                        backgroundColor: '#F52273',
-                    }
+    // components: {
+    //     MuiButton: {
+    //         styleOverrides: {
+    //             root: {
+    //                 color: '#FFF',
+    //                 backgroundColor: '#7e4d72',
+    //                 textTransform: 'none',
+    //                 fontWeight: 'bold',
+    //                 paddingTop: '12px',
+    //                 paddingBottom: '12px',
+    //                 ":hover": {
+    //                     backgroundColor: '#662054',
+    //                 }
 
-                },
-            },
-        },
-        MuiTextField: {
-            defaultProps: {
-                InputLabelProps: {
-                    required: false,
-                },
-                required: true,
-            },
-        },
-    },
+    //             },
+    //         },
+    //     },
+    //     MuiTextField: {
+    //         defaultProps: {
+    //             InputLabelProps: {
+    //                 required: false,
+    //             },
+    //             required: true,
+    //         },
+    //     },
+    // },
 });
 
 export default theme;
