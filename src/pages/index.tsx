cd0899b4f8fc0fdd17f18249ab75519c440f4ebc
vortex2jm <joaopaulomoura5328@gmail.com>
2022-10-06 09:01:44 -0300

    :recycle: Putting header into app.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
 import type { NextPage } from 'next'
-import styles from '../styles/Home.module.css'
-import Header from '../components/Header'
 
 const Home: NextPage = () => {
   return (
-    <div>
-      <Header/>
-    </div>
+    <>
+      <div>
+        Conteudo
+      </div>
+    </>
   )
 }
 
