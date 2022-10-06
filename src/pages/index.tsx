91602baf842d3c594cf264fe944f690c88dea126
vortex2jm <joaopaulomoura5328@gmail.com>
2022-10-06 18:01:53 -0300

    :sparkles: Adding teacher's array

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,33 @@
-import { sliderUnstyledClasses } from '@mui/base'
-import type { NextPage } from 'next'
-import List from '../components/List'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import { Teacher } from "../@types/teacher";
+import List from "../components/List";
+import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
+  const teachers: Teacher[] = [
+    {
+      id: 1,
+      name: "Sr Clevelares",
+      value: "R$ 400,00 por hora",
+      description: "Professor de programação",
+      photo: "/images/professor.jpg"
+    },
+    {
+      id: 2,
+      name: "Kevin Space",
+      value: "R$ 450,00 por hora",
+      description: "Professor de Brabeza language",
+      photo: "/images/professor.jpg"
+    }
+  ];
+
   return (
     <>
       <div className={styles.container}>
-        <List/>
+        <List teachers={teachers} />
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
