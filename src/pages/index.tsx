4bc132369ab0063dccec74a07436d5510c2e9b32
vortex2jm <joaopaulomoura5328@gmail.com>
2022-10-06 14:19:10 -0300

    :recycle: Adjusting proportions and adding background-color property

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
+import { sliderUnstyledClasses } from '@mui/base'
 import type { NextPage } from 'next'
 import List from '../components/List'
+import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
     <>
-      <div>
+      <div className={styles.container}>
         <List/>
       </div>
     </>
