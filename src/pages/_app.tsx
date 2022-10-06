b911c3b04a9b53eda023bca43e8feab0fbc0624c
vortex2jm <joaopaulomoura5328@gmail.com>
2022-10-06 09:11:56 -0300

    :sparkles: Setting up global theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,16 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Header from '../components/Header'
+import { ThemeProvider } from '@mui/material'
+import theme from '../themes/theme'
+
 
 function MyApp({ Component, pageProps }: AppProps) {
   return(
-    <>
+    <ThemeProvider theme={theme}>
       <Header/>
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   ) 
 }
 
