30225cdeffbb58fd3e6356eb7f9d4c2a668bfad2
vortex2jm <joaopaulomoura5328@gmail.com>
2022-10-06 18:12:31 -0300

    :fire: Removing comments

--- src/themes/theme.ts
+++ src/themes/theme.ts
@@ -14,33 +14,7 @@ const theme = createTheme({
             primary: '#F52273',
             secondary: '#6c6767',
         },
-    },
-    // components: {
-    //     MuiButton: {
-    //         styleOverrides: {
-    //             root: {
-    //                 color: '#FFF',
-    //                 backgroundColor: '#7e4d72',
-    //                 textTransform: 'none',
-    //                 fontWeight: 'bold',
-    //                 paddingTop: '12px',
-    //                 paddingBottom: '12px',
-    //                 ":hover": {
-    //                     backgroundColor: '#662054',
-    //                 }
-
-    //             },
-    //         },
-    //     },
-    //     MuiTextField: {
-    //         defaultProps: {
-    //             InputLabelProps: {
-    //                 required: false,
-    //             },
-    //             required: true,
-    //         },
-    //     },
-    // },
+    }
 });
 
 export default theme;
